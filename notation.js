56d9f7150309fc03bdc8014017322ad652c03d29
Shojjoti <shojjotihhossenbd30@gmail.com>
2025-03-12 04:05:29 +0600

    finish

---
+++ notation.js
@@ -0,0 +1,15 @@
+const student = {
+    name: 'Khan' , 
+    age: 20 , 
+    movies: ['king khan' , 'Jon']
+}
+
+//dot notation 
+const peo1 =  student.age ; 
+
+//bracket notation 
+const peo2 = student["age"]; 
+const pname = 'age' ; 
+const peo3 = student[pname] ; 
+
+console.log(peo3) ;  
---
+++ shortCut.js
@@ -0,0 +1,15 @@
+//truthy : 'name' , 5, true , {} , []
+// falsy: ' ' , 0 , false , null , undefine 
+
+//checking truthy
+let myVar = 5 ;
+if(myVar)
+{
+    myVar = myVar*5 ; 
+}
+else{
+    myVar = 0 ; 
+}
+
+//checking falsy 
+let 
